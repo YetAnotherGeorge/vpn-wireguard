285ea2b6a3b913f5d33457fc651a7dc0bc3df01f
George Legun <georgelegun@gmail.com>
2024-12-09 16:03:21 +0200

    use ubuntuutils package

--- wireguard-install-host/install.py
+++ wireguard-install-host/install.py
@@ -12,8 +12,11 @@ import shutil
 import time
 import re
 import random
-import custom_cmd_util as ccu
+import ubuntuutils;
+import ubuntuutils.uu_io
+from ubuntuutils.uu_run_command_container import RCC
 import json
+
 
 def main():
    #region INIT
@@ -27,7 +30,8 @@ def main():
       return 0
 
    #read config
-   conf = json.loads(ccu.file_read(PATH_CONF))
+   ubuntuutils.uu_run_command_container.RCC
+   conf = json.loads(ubuntuutils.uu_io.file_read(PATH_CONF))
    SERVER_ADDR = str(conf["server_address"])
    SERVER_PORT = int(conf["server_port"])
    SERVER_PEER_COUNT = int(conf["server_peer_count"])
@@ -40,31 +44,31 @@ def main():
    #endregion
 
    # Install Wireguard
-   if ccu.RunCommandContainer("dpkg -l wireguard", True).return_code != 0:
+   if RCC("dpkg -l wireguard", True).return_code != 0:
       print(f"Wireguard is not installed")
-      ccu.RunCommandContainer("apt install wireguard -y").Check()
+      RCC("apt install wireguard -y").Check()
    else:
       print(f"Wireguard is installed -> wg down")
-      ccu.RunCommandContainer("wg-quick down wg0").Check()
+      RCC("wg-quick down wg0").Check()
    
    # Install qrencode
-   if ccu.RunCommandContainer("dpkg -l qrencode", True).return_code != 0:
+   if RCC("dpkg -l qrencode", True).return_code != 0:
       print(f"qrencode is not installed")
-      ccu.RunCommandContainer("apt install qrencode -y").Check()
+      RCC("apt install qrencode -y").Check()
    
    # FIREWALL (UFW)
-   if ccu.RunCommandContainer("dpkg -l ufw", True).return_code != 0:
+   if RCC("dpkg -l ufw", True).return_code != 0:
       raise Exception("THIS SCRIPT ONLY WORKS WITH UFW - missing")
-   ccu.RunCommandContainer(f"bash -c 'yes | ufw enable'")
-   ccu.RunCommandContainer(f"ufw delete allow {SERVER_PORT}").Check()
-   ccu.RunCommandContainer(f"ufw allow {SERVER_PORT} comment \"Wireguard\"").Check()
+   RCC(f"bash -c 'yes | ufw enable'")
+   RCC(f"ufw delete allow {SERVER_PORT}").Check()
+   RCC(f"ufw allow {SERVER_PORT} comment \"Wireguard\"").Check()
    
    # region SYSCTL
    # Read sysctl config 
    sysctl_path = "/etc/sysctl.conf"
    if not os.path.exists(sysctl_path):
       raise Exception(f"missing: \"{sysctl_path}\"")
-   sysctl_contents = ccu.file_read(sysctl_path)
+   sysctl_contents = ubuntuutils.uu_io.file_read(sysctl_path)
    any_change = False
    # Write forwarding rules: IPv4
    if re.search(r"^ \s* net\.ipv4\.ip_forward \s* = \s* 1 \s* $", sysctl_contents, re.M | re.X) == None:
@@ -76,24 +80,24 @@ def main():
       any_change = True
       
    if any_change:
-      ccu.file_write(sysctl_path, sysctl_contents)
+      ubuntuutils.uu_io.file_write(sysctl_path, sysctl_contents)
       print(f"Wrote new sysctl config")
-      ccu.RunCommandContainer("sysctl -p").Check()
+      RCC("sysctl -p").Check()
    #endregion
    
    # check provided interface in defaults
-   iface_cmd = ccu.RunCommandContainer("ip route list default", True).Check()
+   iface_cmd = RCC("ip route list default", True).Check()
    if INTERFACE not in iface_cmd.std_out: 
       raise Exception(f"No such default interface: \"{INTERFACE}\"")
    
    # GRAB DNS
-   dns_ip_list = ccu.RunCommandContainer(f"resolvectl dns {INTERFACE}").Check().std_out
+   dns_ip_list = RCC(f"resolvectl dns {INTERFACE}").Check().std_out
    dns_ip_list_match = re.match(r"Link \s* \d \s* \( [^\)]+ \)\:\s* (?P<di> .+? ) $", dns_ip_list, re.X | re.M)
    dns_ip_list = ", ".join( dns_ip_list_match.group("di").split(" ") )
    print(f"DNS IP LIST: \"{dns_ip_list}\"")
    
    #region Configure Wireguard
-   ccu.RunCommandContainer("systemctl stop wg-quick@wg0")
+   RCC("systemctl stop wg-quick@wg0")
    wireguard_path = "/etc/wireguard"
    conf_path = os.path.join(wireguard_path, "wg0.conf") # CONF DIR IS COMPLETELY REMOVED ON EVERY RUN
    private_key_path = os.path.join(wireguard_path, "private.key")
@@ -101,12 +105,12 @@ def main():
    if not os.path.exists(wireguard_path):
       raise Exception(f"wireguard folder missing: {wireguard_path}")
    print(f"Removing contents of \"{wireguard_path}\" directory...")
-   ccu.clear_directory(wireguard_path)
+   ubuntuutils.uu_io.clear_directory(wireguard_path)
    
-   private_key: str = ccu.RunCommandContainer("wg genkey").Check().std_out.strip()
-   public_key: str = ccu.RunCommandContainer(f"bash -c 'echo \"{private_key}\" | wg pubkey'").Check().std_out.strip()
-   ccu.file_write(path=private_key_path, contents=private_key, permission_bits=0o600)
-   ccu.file_write(path=public_key_path, contents=public_key, permission_bits=0o600)
+   private_key: str = RCC("wg genkey").Check().std_out.strip()
+   public_key: str = RCC(f"bash -c 'echo \"{private_key}\" | wg pubkey'").Check().std_out.strip()
+   ubuntuutils.uu_io.file_write(path=private_key_path, contents=private_key, permission_bits=0o600)
+   ubuntuutils.uu_io.file_write(path=public_key_path, contents=public_key, permission_bits=0o600)
    print(f"SRV Private Key: {re.sub('.', '*', private_key)} | SRV Public Key: {public_key}")
    
    
@@ -133,10 +137,10 @@ def main():
    wg0_conf = "\n".join( [l.strip() for l in wg0_conf.splitlines()] ).strip()
    wg0_conf_print= "\n".join(f"   CONF>  {l}" for l in wg0_conf.replace(private_key, "...PRIVATE_KEY...").splitlines())
    print(f"Writing config: \n{wg0_conf_print}")
-   ccu.file_write(path=conf_path, contents=wg0_conf)
+   ubuntuutils.uu_io.file_write(path=conf_path, contents=wg0_conf)
    
-   ccu.RunCommandContainer("systemctl enable wg-quick@wg0").Check()
-   ccu.RunCommandContainer("systemctl restart wg-quick@wg0").Check()
+   RCC("systemctl enable wg-quick@wg0").Check()
+   RCC("systemctl restart wg-quick@wg0").Check()
    #endregion
    
    #region Configure Wireguard Peers
@@ -151,12 +155,12 @@ def main():
       os.mkdir(peer_dir)
       
       # Public, Private keys
-      peer_private_key = ccu.RunCommandContainer("wg genkey").Check().std_out.strip()
-      peer_public_key = ccu.RunCommandContainer(f"bash -c 'echo \"{peer_private_key}\" | wg pubkey'").Check().std_out.strip()
+      peer_private_key = RCC("wg genkey").Check().std_out.strip()
+      peer_public_key = RCC(f"bash -c 'echo \"{peer_private_key}\" | wg pubkey'").Check().std_out.strip()
       peer_ip4 = f"{ip_v4_str}.{(i+1)}"
       peer_ip6 = f"{ip_v6_str}::{(i+1)}"
-      ccu.file_write(path=peer_private_key_path, contents=peer_private_key, permission_bits=0o600)
-      ccu.file_write(path=peer_public_key_path, contents=peer_public_key, permission_bits=0o600)
+      ubuntuutils.uu_io.file_write(path=peer_private_key_path, contents=peer_private_key, permission_bits=0o600)
+      ubuntuutils.uu_io.file_write(path=peer_public_key_path, contents=peer_public_key, permission_bits=0o600)
       
       # Conf with dns
       # Note: 0.0.0.0/0, ::/0 means all traffic will must go through the vpn connection
@@ -176,10 +180,10 @@ def main():
       peer_conf = "\n".join( [l.strip() for l in peer_conf.splitlines()] ).strip()
       peer_conf_print= "\n".join(f"   CONF (P)>  {l}" for l in peer_conf.replace(private_key, "...PRIVATE_KEY...").splitlines())
       print(f"Writing config: \n{peer_conf_print}")
-      ccu.file_write(path=peer_conf_path, contents=peer_conf, permission_bits=0o600)
+      ubuntuutils.uu_io.file_write(path=peer_conf_path, contents=peer_conf, permission_bits=0o600)
       
-      ccu.RunCommandContainer(f"wg set wg0 peer {peer_public_key} allowed-ips {peer_ip4},{peer_ip6}").Check()
-      ccu.RunCommandContainer(f"qrencode -t ansiutf8 -r \"{peer_conf_path}\" -o \"{peer_conf_qr_path}\"")
+      RCC(f"wg set wg0 peer {peer_public_key} allowed-ips {peer_ip4},{peer_ip6}").Check()
+      RCC(f"qrencode -t ansiutf8 -r \"{peer_conf_path}\" -o \"{peer_conf_qr_path}\"")
    #endregion
    
    
